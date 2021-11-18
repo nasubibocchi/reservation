da1aea69a454949cce24f1036e68f099fd5f055c
nasubibocchi <matsuhito125@gmail.com>
2021-11-18 10:43:24 +0900

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'pages/calendar_page/calendar_page.dart';
 import 'pages/calendar_page/default_calendar_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: MyApp()));
 }
 
